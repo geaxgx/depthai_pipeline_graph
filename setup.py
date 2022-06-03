fcc0b17ea41d35799a7638c1a46cb15ec18d913c
geaxgx <geaxgx@gmail.com>
2022-06-03 14:07:40 +0200

    init

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.readlines()
+
+setup(
+    name="depthai_pipeline_graph",
+    version="0.0.1",
+    packages=[*find_packages()],
+    entry_points={
+        'console_scripts': [
+            'pipeline_graph = depthai_pipeline_graph.pipeline_graph:main'
+        ]
+    },
+    author="geaxgx",
+    include_package_data=True,
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description="Tool to create graphs oh DepthAI pipelines",
+
+
+    python_requires=">=3.6",
+    install_requires=list(open("requirements.txt")),
+)
