2dfa8982d2df49763c3108ce623ef862c2c359f1
geaxgx <geaxgx@gmail.com>
2022-07-19 22:11:31 +0200

    Add 'from_file' argument and capability to create the pipeline graph from a file (log or serializeToJson() file)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="depthai_pipeline_graph",
-    version="0.0.2",
+    version="0.0.3",
     packages=[*find_packages()],
     entry_points={
         'console_scripts': [
